30a517075dc459b71e09d4ec2694573e2a8cc234
Asanka Perera <gynasanka@gmail.com>
2020-10-31 00:11:49 +1030

    update

--- download_from_gdrive.py
+++ download_from_gdrive.py
@@ -1,7 +1,7 @@
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
 gdd.download_file_from_google_drive(file_id='1jGtYIExXrUq7YxZaeAhwtbbkpZrv5rrN',
-                                    dest_path='/content/TrainYourOwnYOLO/Data/',
+                                    dest_path='/Data',
                                     unzip=True)
 
                                    # https://drive.google.com/file/d/1jGtYIExXrUq7YxZaeAhwtbbkpZrv5rrN/view?usp=sharing
