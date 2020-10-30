a342fefc63418bb12ef912b9b477638a3aff01fc
Asanka Perera <gynasanka@gmail.com>
2020-10-30 23:44:36 +1030

    update

--- download_from_gdrive.py
+++ download_from_gdrive.py
@@ -1,9 +1,17 @@
-from google_drive_downloader import GoogleDriveDownloader as gdd
+# from google_drive_downloader import GoogleDriveDownloader as gdd
 
-gdd.download_file_from_google_drive(file_id='17AecqojNwKaqQB2cHKXCWx6GERpwkGNM',
-                                    dest_path='./Data/temp1',
-                                    unzip=True)
+# gdd.download_file_from_google_drive(file_id='17AecqojNwKaqQB2cHKXCWx6GERpwkGNM',
+#                                     dest_path='./Data/temp1',
+#                                     unzip=True)
 
                                    # https://drive.google.com/file/d/1jGtYIExXrUq7YxZaeAhwtbbkpZrv5rrN/view?usp=sharing
 
                                    #https://drive.google.com/drive/folders/17AecqojNwKaqQB2cHKXCWx6GERpwkGNM?usp=sharing
+
+
+
+import gdown
+
+url = 'https://drive.google.com/drive/folders/17AecqojNwKaqQB2cHKXCWx6GERpwkGNM?usp=sharing'
+output = 'temp1'
+gdown.download(url, output, quiet=False) 
