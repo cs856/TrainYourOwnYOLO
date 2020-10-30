a8a15476b25d77a5a1dc8bbc7217844a75275f71
Asanka Perera <gynasanka@gmail.com>
2020-10-31 00:16:25 +1030

    update

--- download_from_gdrive.py
+++ download_from_gdrive.py
@@ -13,5 +13,5 @@
 import gdown
 
 url = 'https://drive.google.com/drive/folders/17AecqojNwKaqQB2cHKXCWx6GERpwkGNM?usp=sharing'
-output = '/Data'
+output = './Data'
 gdown.download(url, output, quiet=False) 
