4b88f07ee9438806c84f7ce44cab26244e420bdd
Asanka Perera <gynasanka@gmail.com>
2020-10-31 00:18:16 +1030

    update

--- download_from_gdrive.py
+++ download_from_gdrive.py
@@ -1,17 +1,17 @@
-# from google_drive_downloader import GoogleDriveDownloader as gdd
+from google_drive_downloader import GoogleDriveDownloader as gdd
 
-# gdd.download_file_from_google_drive(file_id='1jGtYIExXrUq7YxZaeAhwtbbkpZrv5rrN',
-#                                     dest_path='/Data',
-#                                     unzip=True)
+gdd.download_file_from_google_drive(file_id='1jGtYIExXrUq7YxZaeAhwtbbkpZrv5rrN',
+                                    dest_path='./Data',
+                                    unzip=True)
 
-#                                    # https://drive.google.com/file/d/1jGtYIExXrUq7YxZaeAhwtbbkpZrv5rrN/view?usp=sharing
+                                   # https://drive.google.com/file/d/1jGtYIExXrUq7YxZaeAhwtbbkpZrv5rrN/view?usp=sharing
 
-#                                    #https://drive.google.com/drive/folders/17AecqojNwKaqQB2cHKXCWx6GERpwkGNM?usp=sharing
+                                   #https://drive.google.com/drive/folders/17AecqojNwKaqQB2cHKXCWx6GERpwkGNM?usp=sharing
 
 
 
-import gdown
+# import gdown
 
-url = 'https://drive.google.com/drive/folders/17AecqojNwKaqQB2cHKXCWx6GERpwkGNM?usp=sharing'
-output = './Data'
-gdown.download(url, output, quiet=False) 
+# url = 'https://drive.google.com/drive/folders/17AecqojNwKaqQB2cHKXCWx6GERpwkGNM?usp=sharing'
+# output = '/Data'
+# gdown.download(url, output, quiet=False) 
