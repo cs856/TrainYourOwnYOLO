d3c347f411bd98fbeb27d2a5bab70efc680d3938
Asanka Perera <gynasanka@gmail.com>
2020-10-30 23:35:03 +1030

    update

--- download_from_gdrive.py
+++ download_from_gdrive.py
@@ -1,7 +1,9 @@
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
-gdd.download_file_from_google_drive(file_id='1jGtYIExXrUq7YxZaeAhwtbbkpZrv5rrN',
-                                    dest_path='./Data/temp.zip',
-                                    unzip=True)
+gdd.download_file_from_google_drive(file_id='17AecqojNwKaqQB2cHKXCWx6GERpwkGNM',
+                                    dest_path='./Data/temp1',
+                                    unzip=False)
 
                                    # https://drive.google.com/file/d/1jGtYIExXrUq7YxZaeAhwtbbkpZrv5rrN/view?usp=sharing
+
+                                   #https://drive.google.com/drive/folders/17AecqojNwKaqQB2cHKXCWx6GERpwkGNM?usp=sharing
