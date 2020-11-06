b6a1ce7b29a80a43cea348c8023a6d4fa08d0e66
Asanka Perera <gynasanka@gmail.com>
2020-11-06 13:46:43 +1030

    update

--- download_from_gdrive.py
+++ download_from_gdrive.py
@@ -3,33 +3,31 @@ import gdown
 import zipfile
 from google.colab import files
 
+
+############ baby detector ############
 #download test_vids.zip
-url = 'https://drive.google.com/uc?id=19OVZj6qRr9ygzlZuLaEoLjX584LaYESi'
-output = 'test_vids.zip'
-gdown.download(url, output, quiet=False) 
+# url = 'https://drive.google.com/uc?id=19OVZj6qRr9ygzlZuLaEoLjX584LaYESi'
+# output = 'test_vids.zip'
+# gdown.download(url, output, quiet=False) 
 
-#extract test_vids.zip
-zip = zipfile.ZipFile("test_vids.zip")
-zip.extractall("./Data/Source_Images")
+# #extract test_vids.zip
+# zip = zipfile.ZipFile("test_vids.zip")
+# zip.extractall("./Data/Source_Images")
 
+# #download data_classes.txt
+# url = 'https://drive.google.com/uc?id=1G_GgucCbskWZq5umyMS8lVSe3692Ngkc'
+# output = './Data/Model_Weights/data_classes.txt'
+# gdown.download(url, output, quiet=False) 
 
-#download data_classes.txt
-url = 'https://drive.google.com/uc?id=1G_GgucCbskWZq5umyMS8lVSe3692Ngkc'
-output = './Data/Model_Weights/data_classes.txt'
-gdown.download(url, output, quiet=False) 
-
-
-# #download the trained final weights - fullbody
-# url = 'https://drive.google.com/uc?id=1TRjpUdYQd7RKfmHhm_1oB0u_WIAAs_LY'
+# #download the trained final weights - uppper body
+# url = 'https://drive.google.com/uc?id=1hXy_1_gBaQLiH9ef78Nhg1EB1MUq6I9x'
 # output = './Data/Model_Weights/trained_weights_final.h5'
 # gdown.download(url, output, quiet=False) 
 
-#download the trained final weights - uppper body
-url = 'https://drive.google.com/uc?id=1hXy_1_gBaQLiH9ef78Nhg1EB1MUq6I9x'
+
+############ facemask detector ############
+
+#download the trained final weights
+url = 'https://drive.google.com/uc?id=1tM0rr5terSxNj24NJXfrT-sJleJ2tm_3'
 output = './Data/Model_Weights/trained_weights_final.h5'
 gdown.download(url, output, quiet=False) 
-
-
-
-# files.download('./Data/Model_Weights/data_classes.txt') 
-#files.download('requirements.txt') 
