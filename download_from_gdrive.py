3c70f09a6a87c6495cd1439408a7ca4aea2f1685
Asanka Perera <gynasanka@gmail.com>
2020-11-16 16:11:06 +1030

    update

--- download_from_gdrive.py
+++ download_from_gdrive.py
@@ -4,27 +4,6 @@ import zipfile
 from google.colab import files
 
 
-############ baby detector ############
-# # download test_vids.zip
-# url = 'https://drive.google.com/uc?id=19OVZj6qRr9ygzlZuLaEoLjX584LaYESi'
-# output = 'test_vids.zip'
-# gdown.download(url, output, quiet=False) 
-
-# #extract test_vids.zip
-# zip = zipfile.ZipFile("test_vids.zip")
-# zip.extractall("./Data/Source_Images")
-
-# #download data_classes.txt
-# url = 'https://drive.google.com/uc?id=1G_GgucCbskWZq5umyMS8lVSe3692Ngkc'
-# output = './Data/Model_Weights/data_classes.txt'
-# gdown.download(url, output, quiet=False) 
-
-# #download the trained final weights - uppper body
-# url = 'https://drive.google.com/uc?id=1hXy_1_gBaQLiH9ef78Nhg1EB1MUq6I9x'
-# output = './Data/Model_Weights/trained_weights_final.h5'
-# gdown.download(url, output, quiet=False) 
-
-
 ############ facemask detector ############
 #download test_vids.zip
 url = 'https://drive.google.com/uc?id=1qWVqrdUX4x4Nff1IW0_-BNgIh18Ua32Z'
