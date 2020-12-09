a14ae6a07e4ee90d9af59c60bfa4039fe430befa
Asanka Perera <gynasanka@gmail.com>
2020-12-09 16:55:22 +1030

    train whole model

--- 2_Training/Train_YOLO.py
+++ 2_Training/Train_YOLO.py
@@ -285,7 +285,8 @@ if __name__ == "__main__":
             initial_epoch=epoch1,
             callbacks=[logging, checkpoint, reduce_lr, early_stopping],
         )
-        model.save_weights(os.path.join(log_dir, "trained_weights_final.h5"))
+        #model.save_weights(os.path.join(log_dir, "trained_weights_final.h5"))
+        model.save(os.path.join(log_dir, "trained_whole_model_final.h5"))
         step2_train_loss = history.history["loss"]
 
         file = open(os.path.join(log_dir_time, "step2_loss.npy"), "w")
